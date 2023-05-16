d0d3fc000c4e5391775a7ccfb8f0d94c21186196
Fernando Silva <fernando.silvabr66@gmail.com>
2023-05-15 23:01:18 -0300

    create basic pages to the blog

---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,9 @@
+const ArticlesPage = () => {
+    return (
+        <div>
+        <h1>This is the articles page!</h1>
+        </div>
+    );
+}
+
+export default ArticlesPage;
---
+++ src/pages/ArticlesListPage.js
@@ -0,0 +1,9 @@
+const ArticlesListPage = () => {
+    return (
+        <div>
+        <h1>This is the articles list page!</h1>
+        </div>
+    );
+}
+
+export default ArticlesListPage;
