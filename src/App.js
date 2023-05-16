1bc55a4fdd426009c96ea868d5996bf90cc28f26
Fernando Silva <fernando.silvabr66@gmail.com>
2023-05-15 23:06:35 -0300

    add basic components for pages into App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
     <div className="App">
       <h1>My blog</h1>
       <div className="page-body">
-        Welcome to my blog!
+        //Welcome to my blog!
+        <HomePage />
+        <AboutPage />
+        <ArticlesListPage />
+        <ArticlePage />
       </div>
     </div>
   );
